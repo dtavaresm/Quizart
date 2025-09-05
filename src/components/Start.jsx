06dcae30ace2704d4ce0b1dbf53fb22625288f26
dtavaresm <tavares.machado.debora@gmail.com>
2025-09-05 17:05:23 +0100

    Add End component and update Quiz and Start components to adapt to new End component

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -3,7 +3,7 @@ export default function Start({ onStart }) {
     return (
         <div className="flex flex-col items-center justify-center h-screen gap-6 text-center">
             <p className="text-gray-600">Welcome to the</p>
-            <h1 className="text-4xl font-bold">Art Quiz</h1>
+            <h1 className="text-4xl font-bold">Quizart</h1>
             <p className="text-gray-600">Choose your theme to begin</p>
             <div className="flex gap-4">
                 <button
@@ -16,7 +16,7 @@ export default function Start({ onStart }) {
                     onClick={() => onStart("movement")}
                      className="px-6 py-3 w-35 bg-blue-500 text-white rounded-lg hover:bg-blue-600 cursor-pointer"
                 >
-                    Movements
+                    Art Movements
                 </button>
             </div>
         </div>
